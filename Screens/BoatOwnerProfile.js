81b62e79fac4c37e4054694fea6ead7d0537865d
Fxkhalil0 <118560438+Fxkhalil0@users.noreply.github.com>
2023-06-11 13:15:15 -0800

    update

---
+++ Screens/BoatOwnerProfile.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class BoatOwnerProfile extends Component {
+  render() {
+    return (
+      <View>
+        <Text>BoatOwnerProfile</Text>
+      </View>
+    )
+  }
+}
+
+export default BoatOwnerProfile
