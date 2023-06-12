25eb72c1948c9d17e92c064968f3c6d01bd0e113
kerolos khairy <132648652+keroloskhairyy@users.noreply.github.com>
2023-06-12 16:20:29 -0700

    login-signUpScreen & OnBoarding Screen

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+<<<<<<< Updated upstream
 import {Discreption} from './Screens/Discreption'
+=======
+// import OnboardingScreen from './Screens/OnboardingScreen';
+import LoginSignUp from './Screens/LoginSignUpScreen/LoginSignUp';
+
+>>>>>>> Stashed changes
 export default function App() {
   return (
+    
     <View style={styles.container}>
+<<<<<<< Updated upstream
       <Discreption></Discreption>
     </View>
   );
+=======
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+
+  )
+>>>>>>> Stashed changes
 }
 
 const styles = StyleSheet.create({
