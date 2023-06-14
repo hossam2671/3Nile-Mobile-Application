823f47b3d538a654d6a7d2c1d89de5d33d8b2c71
Fxkhalil0 <118560438+Fxkhalil0@users.noreply.github.com>
2023-06-14 09:03:10 -0800

    ...

--- App.js
+++ App.js
@@ -1,44 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native';
-// import UserProfile from './Screens/UserProfile'
-// import BoatOwnerProfile from './Screens/BoatOwnerProfile';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Icon from 'react-native-vector-icons/Ionicons';
+import UserProfile from './Screens/UserProfile'
+import BoatOwnerProfile from './Screens/BoatOwnerProfile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import BottomNavigatTab from './Screens/BottomNavigatTab';
+import Icon from 'react-native-vector-icons/Ionicons';
 import HomeCards from './Screens/HomeCards';
-
-// const Tab = createBottomTabNavigator();
-
 import {Discreption} from './Screens/Discreption'
 // import OnboardingScreen from './Screens/OnboardingScreen';
 import LoginSignUp from './Screens/LoginSignUpScreen/LoginSignUp';
 import Filter from './Screens/Filter';
 import OnboardingScreen from './Screens/OnboardingScreen';
+import Splash from './Screens/Splash';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <HomeCards />
+    // <HomeCards />
 
-//     <NavigationContainer>
-//     <Tab.Navigator screenOptions={{
-//           tabBarInactiveTintColor: '#6f6f6f',
-//           tabBarActiveTintColor: '#00409f',
-//           tabBarStyle: { backgroundColor: '#ffffff' },
-//         }}>
-    
-//     <Tab.Screen 
-//   name='Owner' 
-//   component={BoatOwnerProfile}
-//   options={{ tabBarIcon: ({ color }) => <Icon name="man" color={color} size={24} /> }}
-// />
+    <NavigationContainer>
+     <Stack.Navigator screenOptions={{
+        headerStyle: { backgroundColor: '#ffffff' },
 
-// <Tab.Screen 
-//   name='User' 
-//   component={UserProfile}
-//   options={{ tabBarIcon: ({ color }) => <Icon name="man" color={color} size={24} /> }}
-// />
-//     </Tab.Navigator>
-//   </NavigationContainer>
+      }}>
+        <Stack.Screen options={{ headerShown: false }} name='splash' component={Splash} />
+        <Stack.Screen options={{ headerShown: false }} name='HomeCards' component={BottomNavigatTab}></Stack.Screen>
+
+      </Stack.Navigator>
+  </NavigationContainer>
   )
 }
 
---
+++ Screens/BottomNavigatTab.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IIcon from 'react-native-vector-icons/Entypo';
+import HomeCards from './HomeCards';
+import Filter from './Filter';
+import Discreption from './Discreption';
+import UserProfile from './UserProfile';
+
+
+const Tab = createBottomTabNavigator();
+
+function BottomNavigatTab() {
+  return <Tab.Navigator screenOptions={{
+    tabBarInactiveTintColor: '#6f6f6f',
+    tabBarActiveTintColor: '#00409f',
+    tabBarStyle: { backgroundColor: '#ffffff' },
+  }}>
+
+<Tab.Screen 
+name='home' 
+component={HomeCards}
+options={{ tabBarIcon: ({ color }) => <Icon name="home" color={color} size={24} /> }}
+/>
+
+<Tab.Screen 
+name='search' 
+component={Filter}
+options={{ tabBarIcon: ({ color }) => <Icon name="search" color={color} size={24} /> }}
+/>
+
+<Tab.Screen 
+name='User' 
+component={UserProfile}
+options={{ tabBarIcon: ({ color }) => <Icon name="user" color={color} size={24} /> }}
+/>
+
+<Tab.Screen 
+name='Disc' 
+component={Discreption}
+options={{ tabBarIcon: ({ color }) => <IIcon name="chat" color={color} size={24} /> }}
+/>
+
+</Tab.Navigator>
+}
+
+export default BottomNavigatTab
